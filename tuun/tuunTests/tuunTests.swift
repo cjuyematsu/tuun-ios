e955d1ff1df2d41a277dfde1f34c83236fe8f379
ColeUyematsu <cjuyematsu@gmail.com>
2025-10-18 13:21:03 -0700

    initial commit

---
+++ tuun/tuunTests/tuunTests.swift
@@ -0,0 +1,17 @@
+//
+//  tuunTests.swift
+//  tuunTests
+//
+//  Created by Cole Uyematsu on 10/18/25.
+//
+
+import Testing
+@testable import tuun
+
+struct tuunTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
