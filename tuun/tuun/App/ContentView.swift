2a356292c656b6a448e95ad633dbef83339e0fe8
ColeUyematsu <cjuyematsu@gmail.com>
2025-10-26 13:58:19 -0700

    Restructure: Move ContentView and tuunApp into App Folder

---
+++ tuun/tuun/App/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  tuun
+//
+//  Created by Cole Uyematsu on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ProfileView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
